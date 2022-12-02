ac76b3871912c9c8f6834aeae01c0126fa4d4182
fariasraphael <farias.raphael@gmail.com>
2022-12-02 00:17:18 -0300

    Acrescento aula 2 - POO - CLASSE LOCADORA E FILME

---
+++ src/modulo02/Main.java
@@ -0,0 +1,28 @@
+package modulo02;
+
+import modulo02.locadora.Filme;
+import modulo02.locadora.Locadora;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Filme marvel = new Filme("Pantera Negra", "Marvel", 7.00);
+        Filme dc = new Filme("Mulher Maravilha", "Heróis", "dc", "10", 7.0);
+        List<Filme> listaFilme = new ArrayList<>();
+        listaFilme.add(marvel);
+        listaFilme.add(dc);
+
+        Locadora locadora = new Locadora("AdaFlix", "123456789", listaFilme);
+        locadora.cadastrarFilme(marvel);
+        locadora.cadastrarFilme(dc);
+        locadora.listarFilmes();
+
+//        Locadora locadora = new Locadora("AdaFlix", "123456789", listaFilme);
+//        locadora.cadastrarFilme(marvel);
+//        locadora.cadastrarFilme(dc);
+//        locadora.listarFilmes();
+    }
+}
---
+++ src/modulo02/locadora/Locadora.java
@@ -0,0 +1,62 @@
+package modulo02.locadora;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Locadora {
+
+    private String nomeLocadora;
+    private String cnpj;
+    private List<Filme> filmes = new ArrayList<>();
+
+    public Locadora(String nomeLocadora, String cnpj, List<Filme> filmes) {
+        this.nomeLocadora = nomeLocadora;
+        this.cnpj = cnpj;
+        this.filmes = filmes;
+    }
+
+    public Locadora(String nomeLocadora, String cnpj) {
+        this.nomeLocadora = nomeLocadora;
+        this.cnpj = cnpj;
+        filmes = new ArrayList<>();
+    }
+
+
+    public Locadora() {}
+
+
+    public String getNomeLocadora() {
+        return nomeLocadora;
+    }
+
+    public void setNomeLocadora(String nomeLocadora) {
+        this.nomeLocadora = nomeLocadora;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public List<Filme> getFilmes() {
+        return filmes;
+    }
+
+    public void setFilmes(List<Filme> filmes) {
+        this.filmes = filmes;
+    }
+
+    public void cadastrarFilme(Filme filme) {
+        this.filmes.add(filme);
+    }
+
+    public void listarFilmes(){
+        for (Filme filme: this.filmes) {
+            System.out.println(filme.toString());
+        }
+    }
+
+}
