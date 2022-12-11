4d4b499586d4b93cfcb403d56a3e7171e8eec4a9
fariasraphael <farias.raphael@gmail.com>
2022-12-11 10:33:55 -0300

    crio classe Funcionario extends Pessoa

--- src/modulo02/POO/heranca/empresa/Funcionario.java
+++ src/modulo02/POO/heranca/empresa/Funcionario.java
@@ -1,4 +1,11 @@
 package modulo02.POO.heranca.empresa;
 
-public class Funcionario {
+public class Funcionario extends Pessoa{
+
+    protected String matriculaFuncionario;
+
+    public Funcionario(String nome, String cpf, String matriculaFuncionario) {
+        super(nome, cpf);
+        this.matriculaFuncionario = matriculaFuncionario;
+    }
 }
--- src/modulo02/POO/heranca/empresa/Pessoa.java
+++ src/modulo02/POO/heranca/empresa/Pessoa.java
@@ -1,4 +1,12 @@
 package modulo02.POO.heranca.empresa;
 
 public class Pessoa {
+    protected String nome;
+    protected String cpf;
+
+    public Pessoa (String nome, String cpf){
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+
 }
