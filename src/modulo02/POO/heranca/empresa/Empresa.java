62dacbf8fca8b4814178a8c7bcfe14bab9cdd8b1
fariasraphael <farias.raphael@gmail.com>
2022-12-11 10:46:59 -0300

    crio classe Empresa e adiciono objeto gerente

--- src/modulo02/POO/heranca/empresa/Empresa.java
+++ src/modulo02/POO/heranca/empresa/Empresa.java
@@ -3,7 +3,7 @@ package modulo02.POO.heranca.empresa;
 public class Empresa {
     public static void main(String[] args) {
 
-        Gerente gerente = new Gerente("Raphael", 123456,"JAVA-945");
+        Gerente gerente = new Gerente("Raphael", "123456","987","JAVA-945");
         System.out.println(gerente.nome);
 
     }
