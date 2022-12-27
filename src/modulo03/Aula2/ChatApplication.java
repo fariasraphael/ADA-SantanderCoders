62840f22fbcc060035681926251f8979f6151a4c
fariasraphael <farias.raphael@gmail.com>
2022-12-26 23:34:43 -0300

    refatoro o codigo criando método generico colocaTSala que cria sala somente com subtipos de Participantes de mesma Classe <T>

--- src/modulo03/Aula2/ChatApplication.java
+++ src/modulo03/Aula2/ChatApplication.java
@@ -1,13 +1,31 @@
 package modulo03.Aula2;
 
 public class ChatApplication {
-    public void colocaParticipanteSala (Sala sala, Participante participante){
+
+    public static void main(String[] args){
+
+        Sala<Aluno> salaAlunos = new Sala<>();
+        Sala<Professor> salaProfessores = new Sala<>();
+
+    }
+    public <T extends Participante> void colocaTSala(Sala<T> sala, T t) {
+        if(sala.adicionaParticipante(t)) {
+            System.out.println("Participante adicionado.");
+        } else {
+            System.out.println("Erro ao adicionar participante.");
+        }
+    }
+
+
+   /*
+   public void colocaParticipanteSala (Sala sala, Participante participante){
         if(sala.adicionaParticipante(participante)) {
             System.out.println("Participante adicionado.");
         } else {
             System.out.println("Erro ao adicionar participante.");
         }
     }
+
     public void colocaAlunoSala(Sala sala, Aluno aluno) {
         //como Aluno estende Participante eu posso usar o metodo colocaParticipanteSala
         // dentro do metodo colocaAlunoSala passando o objeto sala e aluno, reduzindo codigo
@@ -20,7 +38,7 @@ public class ChatApplication {
         colocaParticipanteSala(sala, professor);
     }
 
-    /*public void colocaAlunoSala(Sala sala, Aluno aluno) {
+    public void colocaAlunoSala(Sala sala, Aluno aluno) {
         //o método só aceita aluno devido este estender Participante
         if(sala.adicionaParticipante(aluno)) {
             System.out.println("Aluno adicionado.");
