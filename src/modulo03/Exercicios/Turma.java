91bc52903f5ab6716298f3d5a9d6c3f0445eb633
fariasraphael <farias.raphael@gmail.com>
2022-12-27 17:50:35 -0300

    Torno Classe Turma genérica

--- src/modulo03/Exercicios/Turma.java
+++ src/modulo03/Exercicios/Turma.java
@@ -3,18 +3,18 @@ package modulo03.Exercicios;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Turma {
+public class Turma <T extends Aluno> {
     private Professor professor;
-    private final List<Aluno> alunos = new ArrayList<>();
+    private final List<T> ts = new ArrayList<>();
 
-    public void adicionarAluno (Aluno aluno){
-        alunos.add(aluno);
+    public void adicionarAluno (T t){
+        ts.add(t);
     }
     public void adicionarProfessor (Professor professor){
         this.professor = professor;
     }
     @Override
     public String toString() {
-        return "Professor: " + professor.nome + " | Alunos: " + alunos;
+        return "Professor: " + professor.nome + " | Alunos: " + ts;
     }
 }
