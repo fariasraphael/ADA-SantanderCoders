cf4f4e4a2a661b13d87017d4d0857545f2475151
fariasraphael <farias.raphael@gmail.com>
2022-12-27 18:37:21 -0300

    adiciono Collections.sort(turma.ts); no metodo montarTurma

--- src/modulo03/Exercicios/Sistema.java
+++ src/modulo03/Exercicios/Sistema.java
@@ -1,5 +1,8 @@
 package modulo03.Exercicios;
 
+import java.util.Collection;
+import java.util.Collections;
+
 public class Sistema {
     public static void main(String[] args) {
         Professor professor = new Professor("Kadu");
@@ -8,23 +11,24 @@ public class Sistema {
         AlunoJava alunoJava2 = new AlunoJava("Vivian Java");
         AlunoJava alunoJava3 = new AlunoJava("Raphael Java");
 
-        AlunoJS alunoJS1 = new AlunoJS("Leonardo JS");
+        AlunoJS alunoJS1 = new AlunoJS("Raphael JS");
         AlunoJS alunoJS2 = new AlunoJS("Vivian JS");
-        AlunoJS alunoJS3 = new AlunoJS("Raphael JS");
+        AlunoJS alunoJS3 = new AlunoJS("Leonardo JS");
 
         Turma <AlunoJava> turmaJava = new Turma<>();
         Turma <AlunoJS> turmaJS = new Turma<>();
 
-        criarTurma(turmaJava, professor, alunoJava1, alunoJava2, alunoJava3);
-        criarTurma(turmaJS, professor, alunoJS1, alunoJS2, alunoJS3);
+        montarTurma(turmaJava, professor, alunoJava1, alunoJava2, alunoJava3);
+        montarTurma(turmaJS, professor, alunoJS1, alunoJS2, alunoJS3);
 
         System.out.println(turmaJava);
         System.out.println(turmaJS);
     }
-    public static <T extends Aluno> void criarTurma(Turma<T> turma, Professor professor, T t1, T t2, T t3){
+    public static <T extends Aluno> void montarTurma(Turma<T> turma, Professor professor, T t1, T t2, T t3){
         turma.adicionarProfessor(professor);
         turma.adicionarAluno(t1);
         turma.adicionarAluno(t2);
         turma.adicionarAluno(t3);
+        Collections.sort(turma.ts);
     }
 }
