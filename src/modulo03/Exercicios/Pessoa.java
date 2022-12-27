836c9febb7ca727670cf153f3f5cc196790773bf
fariasraphael <farias.raphael@gmail.com>
2022-12-27 16:43:40 -0300

    Crio classe abstrata Pessoa, seu construtor e sobrescrevo o metodo toString pra retornar somente o nome

--- src/modulo03/Exercicios/Pessoa.java
+++ src/modulo03/Exercicios/Pessoa.java
@@ -1,22 +1,13 @@
 package modulo03.Exercicios;
 
-abstract public class Pessoa implements Comparable<Pessoa>{
-    String nome;
-
-    Pessoa (String nome) {
-    this.nome = nome;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
+abstract public class Pessoa {
+    public String nome;
+    Pessoa(String nome){
         this.nome = nome;
     }
 
     @Override
-    public int compareTo(Pessoa o) {
-        return this.nome.compareTo(o.getNome());
+    public String toString() {
+        return nome;
     }
 }
