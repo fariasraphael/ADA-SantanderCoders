8817c791dbc971ff747e55410dc504f03657bfbf
fariasraphael <farias.raphael@gmail.com>
2022-12-26 22:30:27 -0300

    refatoro o codigo utilizando o metodo colocaParticipanteSala dentro do metodo colocaAlunoSala

--- src/modulo03/Aula2/ChatApplication.java
+++ src/modulo03/Aula2/ChatApplication.java
@@ -8,8 +8,13 @@ public class ChatApplication {
             System.out.println("Erro ao adicionar participante.");
         }
     }
-
     public void colocaAlunoSala(Sala sala, Aluno aluno) {
+        //como Aluno estende Participante eu posso usar o metodo colocaParticipanteSala
+        // dentro do metodo colocaAlunoSala passando o objeto sala e aluno, reduzindo codigo
+        colocaParticipanteSala(sala, aluno);
+    }
+
+    /*public void colocaAlunoSala(Sala sala, Aluno aluno) {
         //o método só aceita aluno devido este estender Participante
         if(sala.adicionaParticipante(aluno)) {
             System.out.println("Aluno adicionado.");
@@ -25,5 +30,5 @@ public class ChatApplication {
         } else {
             System.out.println("Erro ao adicionar Professor.");
         }
-    }
+    }*/
 }
