5af1202674d429f08a7761dad2d482bedd38b0d9
fariasraphael <farias.raphael@gmail.com>
2022-12-26 22:01:54 -0300

    criado metodo adiciona participante que só aceita objeto da classe Participante

--- src/modulo03/Aula2/Sala.java
+++ src/modulo03/Aula2/Sala.java
@@ -3,10 +3,14 @@ package modulo03.Aula2;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Sala <T extends Participante>{
-    private List<T> ts = new ArrayList<>();
+public class Sala {
+    private List<Participante> participantes = new ArrayList<>();
+            /*
+            List<Participante> faz o java só aceitar Participante nessa lista, pois foi passado
+             dentro do parametro '<Generico>'. Se esta estiver vazia, a lista aceitaria qualquer objeto.
+             */
+    public boolean adicionaParticipante(Participante participante) {
+        return participantes.add(participante);
 
-    public boolean adicionaParticipante(T t){
-        return ts.add(t);
     }
 }
