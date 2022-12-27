306f059049b57dacb5410c769607967f6e5014ef
fariasraphael <farias.raphael@gmail.com>
2022-12-27 18:35:56 -0300

    Atualizo enunciado

---
+++ src/modulo03/Exercicios/Enunciado.java
@@ -0,0 +1,43 @@
+package modulo03.Exercicios;
+
+public class Enunciado {
+    /*
+    Sistema de Eduação a Distancia - Parte 1 - 01:41:14
+- Criar a classe Pessoa
+- Criar as classes Aluno, Professor que deverão estender Pessoa
+- Criar a classe turma que terá:
+    - Um Professor
+    -Uma lista de alunos
+- Criar a classe Sistema
+    - implementar um método que inclua alunos em uma turma
+    - implementar o método main
+        - Criar uma turma e colocar um professor e 3 alunos
+
+Sistema de Eduação a Distancia - Parte 1.1 - 02:07:42
+- Criar as classes AlunoJava e AlunosJS que estendem Aluno
+- Fazer com que a Classe sala seja genérica
+- Fazer com que o método de incluir alunos em uma sala seja genérico
+- Criar duas turmas, uma para Java e outra para JavaScript e populá-las com um professor e 3 alunos
+
+Sistema de Eduação a Distancia - Parte 1.2 - 02:36:54
+- Imprimir as turmas com os alunos em ordem alfabética
+	-implementar a interface genérica Comparable na classe pessoa
+
+Sistema de Educação a Distância - Parte 2
+
+- Criar uma classe Secretaria que criará duas turmas (uma de Java e outra de JS)
+    - criar o método matricula() -> responsável por popular as turmas [professor e alunos];
+    - criar o método getAlunoInfo que retornará uma String contendo os dados do aluno;
+    - criar o método que listará os alunos na ordem alfabética (Comparable);
+    - criar o método que listará os alunos na ordem por matrícula (Comparator);
+    - A lista retornada nos dois métodos
+- A lista retornada nos dois métodos anteriores deverá retornar uma lista de Strings contendo o resultado
+      do método getAlunoInfo;
+
+- O método main deverá chamar o método matrícula da Secretaria para inicializar o sistema;
+
+- Criar a classe Supervisao, que terá o método analisaFrequência que utiliza o método de ordem alfabética
+- Criar a classe Pedagogico, que terá o método analisaEvolucaoAcademica que utiliza o método com a lista ordenada
+  por matrícula.
+     */
+}
