0d16304cd39c31a647e4a27dcfc67d81fe65bcbe
fariasraphael <farias.raphael@gmail.com>
2022-12-26 23:44:54 -0300

    anotações de classe genericas

--- src/modulo03/Aula2/Sala.java
+++ src/modulo03/Aula2/Sala.java
@@ -3,6 +3,10 @@ package modulo03.Aula2;
 import java.util.ArrayList;
 import java.util.List;
 
+/*
+<Classe sala se torna genérica quando informo que T extends uma Classe especifica.
+Deste modo ela so aceita a Classe T e seus subtipos.
+*/
 public class Sala <T extends Participante> {
     /*
     faço com que a lista e o metodo da Classe Sala aceite somente Tipos (T = TYPE) que estendam Participante
@@ -14,3 +18,12 @@ public class Sala <T extends Participante> {
 
     }
 }
+
+/*
+classes genéricas não são tão comuns de serem criadas
+normalmente cria-se métodos genéricos
+normalmente cria-se interface para não ter a necessidade de criação de classes ou metodos genericos.
+O generics foi criado para impedir erros do tipo ClassCastException, pois o java faz o casting dos valores passados.
+O genérics aumenta a tipagem do JAVA.
+não exite generics para primitvos, somente para objetos.
+ */
