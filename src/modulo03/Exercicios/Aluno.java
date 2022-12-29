ecd771b36e529cc48c929da7e05416b26fb97793
fariasraphael <farias.raphael@gmail.com>
2022-12-28 22:43:23 -0300

    acrescento atributo matricula e lista de notas na Classe aluno

--- src/modulo03/Exercicios/Aluno.java
+++ src/modulo03/Exercicios/Aluno.java
@@ -1,6 +1,16 @@
 package modulo03.Exercicios;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Aluno extends Pessoa {
-    Aluno (String nome){
+    public String matricula;
+    public List<Double> notas = new ArrayList<>();
+    Aluno (String nome, String matricula){
         super(nome);
+        this.matricula = matricula;
+    }
+    public void addNota (Double nota) {
+        notas.add(nota);
     }
 }
