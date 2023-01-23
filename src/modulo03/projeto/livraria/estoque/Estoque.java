72580d715878935675a17be8b728c92b3dd4881d
fariasraphael <farias.raphael@gmail.com>
2023-01-22 23:22:52 -0300

    Refatoro carrinho de compras, estoque, registro de vendas e comento sistema

--- src/modulo03/projeto/livraria/estoque/Estoque.java
+++ src/modulo03/projeto/livraria/estoque/Estoque.java
@@ -9,6 +9,7 @@ public class Estoque {
     public Collection<Object> getProdutosT() {
         return produtosT;
     }
+
     public <P extends Produto> boolean adicionarItemNoEstoque(P produto) {
         if (this.produtosT.contains(produto)) {
             produto.setQuantidade(produto.getQuantidade() + 1);
@@ -16,45 +17,37 @@ public class Estoque {
         return this.produtosT.add(produto);
     }
 
-    public <P extends Produto> void quantidadeDeItemEmEstoque (P produto) {
-        if(produto.getQuantidade() == null){
+    public <P extends Produto> void quantidadeDeItemEmEstoque(P produto) {
+        if (produto.getQuantidade() == null) {
             System.out.println("Item não cadastrado no estoque");
-        }
-        else {
+        } else {
             System.out.println("Quantidade de " + produto.getNome() + " em estoque: " + produto.getQuantidade() + " unidade(s)");
         }
     }
 
-    public <P extends Produto> void reduzirQuantidadeDoItemNoEstoque (P produto) {
-        if(produto.getQuantidade() == 0 || produto.getQuantidade() == null){
-            System.out.println("Retira do item não realizada");
-            this.quantidadeDeItemEmEstoque(produto);
-        }
-        else{
-            produto.setQuantidade(produto.getQuantidade()-1);
+    public <P extends Produto> void reduzirQuantidadeDoItemNoEstoque(P produto) {
+        if (produto.getQuantidade() == 0 || produto.getQuantidade() == null) {
+            System.out.println("Venda do item " + produto.getNome() + " não realizada, pois não a saldo do item em estoque.");
+        } else {
+            produto.setQuantidade(produto.getQuantidade() - 1);
         }
     }
 
-    public <P extends Produto > Boolean verificadorDeItemEmEstoque(P produto) {
+    public <P extends Produto> Boolean verificadorDeItemEmEstoque(P produto) {
         return this.produtosT.contains(produto);
     }
 
-    public void verTodosOsItensEmEstoque () {
+    public void verTodosOsItensEmEstoque() {
         System.out.println("Todos os produtos em estoque:");
         this.getProdutosT().forEach(System.out::println);
     }
 
-    public void verTodosOsItensDeUmTipoDeEstouqe (String tipo) {
+    public void verTodosOsItensDeUmTipoDeEstouqe(String tipo) {
 
-        for (Object p: produtosT.toArray()) {
-            if (p.getClass().getSimpleName().toString().toUpperCase().equals(tipo.toUpperCase())){
+        for (Object p : produtosT.toArray()) {
+            if (p.getClass().getSimpleName().toString().toUpperCase().equals(tipo.toUpperCase())) {
                 System.out.println(p);
             }
         }
     }
-    @Override
-    public String toString () {
-        return produtosT.toString();
-    }
 }
-
